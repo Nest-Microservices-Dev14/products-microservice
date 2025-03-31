227772291e74f41aeb8fc54316334bafdfc80b95
castillo <fabianboc960@hotmail.com>
2025-03-31 12:27:52 -0500

    feat: :sparkles: Integrar Prisma y actualizar configuración de base de datos
    Se añade Prisma como dependencia en package.json y package-lock.json. Se crea un nuevo archivo de esquema Prisma para la gestión de la base de datos, definiendo el modelo 'Product' y la fuente de datos PostgreSQL. Además, se actualiza la validación de variables de entorno para incluir 'DATABASE_URL'.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
+    DATABASE_URL: string;
 }
 
 const envSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required()
 })
 .unknown(true);
 
@@ -17,5 +19,6 @@ if ( error ) throw new Error(`Config validation error: ${ error.message }`);
 const envVars: EnvVars = value;
 
 export const envs = {
-    PORT: envVars.PORT
+    PORT: envVars.PORT,
+    DATABASE_URL: envVars.DATABASE_URL
 }
