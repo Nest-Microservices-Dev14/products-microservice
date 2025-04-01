5e54e2213c582593ea31ccc29277adacdded462e
castillo <fabianboc960@hotmail.com>
2025-04-01 00:31:20 -0500

    feat: :sparkles: Implementar paginación en el controlador y servicio de productos
    Se añade un nuevo DTO de paginación y se actualizan los métodos del controlador y servicio de productos para soportar la paginación. El método findAll ahora recibe un objeto PaginationDto y devuelve los productos paginados junto con la información de metadatos.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -19,8 +20,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+
+    const { page, limit } = paginationDto;
+    const totalPages = await this.product.count();
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: totalPages / limit
+      }
+    }
+
   }
 
   findOne(id: number) {
