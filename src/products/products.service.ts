775a25c2cc527080d50d8394e9f974e85bbe957e
castillo <fabianboc960@hotmail.com>
2025-04-01 01:10:03 -0500

    feat: :sparkles: Mejorar manejo de productos en el controlador y servicio
    Se actualiza el controlador de productos para utilizar ParseIntPipe en los parámetros de ID, asegurando que se manejen como números. Además, se modifica el servicio de productos para que los métodos findOne y update sean asíncronos y se implementa una excepción NotFoundException para manejar casos en los que no se encuentra un producto. Esto mejora la robustez y la claridad del código al interactuar con la base de datos.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -14,8 +14,8 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     this.logger.log('Database connected');
   }
 
-  create(createProductDto: CreateProductDto) {
-    return this.product.create({
+  async create(createProductDto: CreateProductDto) {
+    return await this.product.create({
       data: createProductDto
     });
   }
@@ -39,12 +39,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id }
+    });
+
+    if ( !product ) throw new NotFoundException(`Product with id ${ id } not found`);
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    await this.findOne( id );
+
+    return await this.product.update({
+      data: updateProductDto,
+      where: { id }
+    });
+
   }
 
   remove(id: number) {
