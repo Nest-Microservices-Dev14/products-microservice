0c61a5c330f06897c44282006654c3260717d13f
castillo <fabianboc960@hotmail.com>
2025-04-08 18:16:14 -0500

    feat: :sparkles: Añadir soporte para NATS y actualizar configuración de entorno
    Se incorpora la dependencia 'nats' en package.json. Se modifica el archivo .env.template para incluir la configuración de NATS, añadiendo 'DATABASE_URL' y 'NATS_SERVERS'. Además, se actualiza el archivo main.ts para utilizar NATS como transporte en el microservicio, mejorando la comunicación entre servicios. También se ajusta la validación de variables de entorno en envs.ts para incluir 'NATS_SERVERS'.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,20 @@ import * as joi from 'joi';
 interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
+    NATS_SERVERS: string[];
 }
 
 const envSchema = joi.object({
     PORT: joi.number().required(),
-    DATABASE_URL: joi.string().required()
+    DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items( joi.string() ).required()
 })
 .unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if ( error ) throw new Error(`Config validation error: ${ error.message }`);
 
@@ -20,5 +25,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
     PORT: envVars.PORT,
-    DATABASE_URL: envVars.DATABASE_URL
+    DATABASE_URL: envVars.DATABASE_URL,
+    natsServers: envVars.NATS_SERVERS
 }
